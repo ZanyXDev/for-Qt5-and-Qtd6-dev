0b29964c4211cf140ff2b5010bf8ace9e99b27e8
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-01-30 12:49:31 +0300

    test script

---
+++ scripts/qt5_amd64_tst_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+
+SRC_VOLUME_NAME="source-storage"
+SDK_VOLUME_NAME="androidsdk-storage"
+QT5_VOLUME_NAME="qt5-binary-storage"
+
+docker run \
+	   -v  ${SRC_VOLUME_NAME}:/usr/local/src \
+       -ti --rm --name git bitnami/git:latest bash -c '
+#!/bin/bash
+set -e
+QT_VERSION="v5.15.10-lts-lgpl"
+REPOSRC="https://invent.kde.org/qt/qt/qt5.git"
+LOCALREPO="/usr/local/src/qt5"
+
+git clone --depth 1 --branch $QT_VERSION \
+		"$REPOSRC" "$LOCALREPO" 2> /dev/null || (git -C "$LOCALREPO" pull)
+cd  $LOCALREPO
+git -c submodule."qt3d".update=none -c submodule."qtactiveqt".update=none -c submodule."qtcanvas3d".update=none  \
+         -c submodule."qtdatavis3d".update=none -c submodule."qtgamepad".update=none -c submodule."qtlottie".update=none    \
+         -c submodule."qtmacextras".update=none -c submodule."qtpim".update=none -c submodule."qtquick3d".update=none   \
+         -c submodule."qtscript".update=none -c submodule."qtscxml".update=none -c submodule."qtspeech".update=none    \
+	 -c submodule."qtvirtualkeyboard".update=none -c submodule."qtwebengine".update=none -c submodule."qtwebglplugin".update=none \
+         -c submodule."qtwebsockets".update=none -c submodule."qtwebview".update=none  -c submodule."qtwinextras".update=none  \
+         -c submodule."qtxmlpatterns".update=none submodule update --init --recursive
+'
+
+docker run \
+	-v ${SRC_VOLUME_NAME}:/usr/local/src:ro \
+	-v ${SDK_VOLUME_NAME}:/opt/android-sdk \
+	-v ${QT5_VOLUME_NAME}:/opt/Qt \
+	-v /home/zanyxdev/git_source/my_src/docker/for-Qt5-and-Qtd6-dev/scripts/inline/build_qt5_amd64.sh:/root/build_qt.sh \
+	-ti --rm zanyxdev/qt5-toolchain:latest bash
