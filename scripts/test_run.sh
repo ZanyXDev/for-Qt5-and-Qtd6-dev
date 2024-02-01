6423eec02e5b177f4c98feb5a7c0f976fc964c28
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-02-01 18:02:38 +0300

    build scripts

--- scripts/test_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- docker run --rm -ti\
-    --user $UID:$GID \
-    --workdir="/home/$USER" \
-    --volume="/etc/group:/etc/group:ro" \
-    --volume="/etc/passwd:/etc/passwd:ro" \
-    --volume="/etc/shadow:/etc/shadow:ro" \
-	zanyxdev/cmake-3.28.1:latest bash
