5d4a2ebcad5035c1f623c52e2c4f4742d9411b0d
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-02-03 12:10:10 +0300

    save

--- scripts/inline/build_qt5_android.sh
+++ scripts/inline/build_qt5_android.sh
@@ -4,28 +4,41 @@ set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo
 pushd .
 trap "popd" EXIT HUP INT QUIT TERM
 
-export VERSION="5.15.10-amd64-lts-lgpl"
+export SDK_PLATFORM=android-31
+export SDK_BUILD_TOOLS=31.0.0
+export MIN_NDK_PLATFORM=android-21
+export ANDROID_NDK_ROOT="/opt/android-sdk/ndk"
+export NDK_VERSION="22.1.7171670"
+export VERSION="5.15.10-android-lts-lgpl"
+export ANDROID_HOME=/opt/android-sdk
+export ANDROID_SDK_ROOT=${ANDROID_HOME}
+export ANDROID_NDK_ROOT=${ANDROID_HOME}/ndk/${NDK_VERSION}
+export ANDROID_NDK_HOST=linux-x86_64
+export ANDROID_NDK_PLATFORM=${MIN_NDK_PLATFORM}
+export ANDROID_API_VERSION=${SDK_PLATFORM}
+export ANDROID_BUILD_TOOLS_REVISION=${SDK_BUILD_TOOLS}
+export ANDROID_NDK=$ANDROID_NDK_ROOT
+export STANDALONE_EXTRA="--stl=libc++"
+
+#rm -R /opt/Qt/${VERSION}
+
 mkdir -p /tmp/build_qt5 
 cd /tmp/build_qt5 
 
-#Solution is to configure qt with -feature-freetype -fontconfig to allow the use of system fonts
-#set QT_QPA_FONTDIR to something suitable, f.ex. /usr/share/fonts/truetype/dejavu (do export QT_QPA_FONTDIR=/usr/share/fonts/truetype/dejavu before running the application)
+/usr/local/src/qt5/configure -opensource -confirm-license -xplatform android-clang -disable-rpath -android-ndk ${ANDROID_NDK_ROOT} -android-sdk ${ANDROID_HOME} \
+-no-warnings-are-errors -nomake tests -nomake examples \
+-qt-freetype -qt-harfbuzz -qt-libjpeg -qt-libpng -qt-pcre -qt-zlib \
+-skip 3d -skip qtdocgallery -skip activeqt -skip canvas3d -skip charts -skip connectivity -skip datavis3d -skip doc -skip gamepad -skip location -skip lottie -skip macextras  \
+-skip networkauth -skip qtwebengine -skip quick3d -skip quicktimeline -skip remoteobjects -skip script -skip scxml -skip sensors -skip serialbus -skip serialport -skip speech \
+-skip virtualkeyboard -skip wayland -skip webchannel -skip webengine -skip webglplugin -skip websockets -skip webview -skip x11extras -skip xmlpatterns -no-feature-d3d12 -ssl \
+-skip winextras \
+OPENSSL_INCDIR='/opt/android-sdk/android_openssl/ssl_1.1/include/' \
+OPENSSL_LIBS_DEBUG="-llibssl -llibcrypto" \
+OPENSSL_LIBS_RELEASE="-llibssl -llibcrypto" \
+-prefix /opt/Qt/${VERSION}
 
-/usr/local/src/qt5/configure -opensource -confirm-license -dbus -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -release \
--reduce-relocations -optimized-qmake -nomake tests -nomake examples -no-feature-d3d12 -skip 3d -skip activeqt -skip canvas3d -skip datavis3d -skip doc -skip gamepad -skip qtdocgallery \
--skip lottie -skip macextras -skip charts -skip quick3d -skip script -skip scxml -skip speech -skip virtualkeyboard -skip qtwebengine -skip webchannel -skip webengine \
--skip webglplugin -skip websockets -skip webview -skip winextras -prefix /opt/Qt/${VERSION} -v -pkg-config
-
-mkdir /opt/Qt/$VERSION/
-
-cp config.summary /opt/Qt/$VERSION/config.summary 
-echo "configure done..." >>/opt/Qt/$VERSION/make.summary
-echo $? >>/opt/Qt/$VERSION/make.summary
 make -j $(nproc) &> /opt/Qt/$VERSION/build.log
-echo $? >>/opt/Qt/$VERSION/make.summary
-echo "make done..." >>/opt/Qt/$VERSION/make.summary
 make -j $(nproc) install 
-echo $? >>/opt/Qt/$VERSION/make.summary
-echo "make install ..." >>/opt/Qt/$VERSION/make.summary
 
+cp config.summary /opt/Qt/$VERSION/config_android.summary 
 popd >& /dev/null
--- scripts/qt5_amd64_tst_build.sh
+++ scripts/qt5_amd64_tst_build.sh
@@ -5,7 +5,7 @@ SRC_VOLUME_NAME="source-storage"
 SDK_VOLUME_NAME="androidsdk-storage"
 QT5_VOLUME_NAME="qt5-binary-storage"
 
-docker run \
+echo docker run \
 	   -v  ${SRC_VOLUME_NAME}:/usr/local/src \
        -ti --rm --name git bitnami/git:latest bash -c '
 #!/bin/bash
@@ -25,10 +25,31 @@ git -c submodule."qt3d".update=none -c submodule."qtactiveqt".update=none -c sub
          -c submodule."qtwebsockets".update=none -c submodule."qtwebview".update=none  -c submodule."qtwinextras".update=none  \
          -c submodule."qtxmlpatterns".update=none submodule update --init --recursive
 '
+echo -e "Clone KDAB android_openssl"
+echo docker run \
+	  -v  ${SDK_VOLUME_NAME}:/usr/local/src \
+      -ti --rm --name git bitnami/git:latest bash -c '
+#!/bin/bash
+set -e
+REPOSRC="https://github.com/KDAB/android_openssl.git"
+LOCALREPO="/opt/android-sdk/android_openssl"
 
+git clone --depth 1 "$REPOSRC" "$LOCALREPO" 2> /dev/null || (git -C "$LOCALREPO" pull)
+'
+
+IMAGE_NAME="zanyxdev/qt5-toolchain:latest"
+echo docker run \
+	  -v ${SDK_VOLUME_NAME}:/opt/android-sdk \
+	  -v ${QT5_VOLUME_NAME}:/opt/Qt \
+	  -v $(pwd)/inline/get_androidsdk.sh:/root/get_androidsdk.sh  \
+      -ti --rm ${IMAGE_NAME} /root/get_androidsdk.sh
+      
 docker run \
 	-v ${SRC_VOLUME_NAME}:/usr/local/src:ro \
 	-v ${SDK_VOLUME_NAME}:/opt/android-sdk \
 	-v ${QT5_VOLUME_NAME}:/opt/Qt \
-	-v /home/zanyxdev/git_source/my_src/docker/for-Qt5-and-Qtd6-dev/scripts/inline/build_qt5_amd64.sh:/root/build_qt.sh \
-	-ti --rm zanyxdev/qt5-toolchain:latest bash
+	-v /home/zanyxdev/git_source/my_src/docker/for-Qt5-and-Qtd6-dev/scripts/inline/build_qt5_android.sh:/root/build_qt.sh \
+	-ti --rm ${IMAGE_NAME}  bash
+
+#/root/build_qt.sh
+
