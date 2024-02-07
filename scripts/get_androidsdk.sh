b02a20521488a107fc596c404d7d256da2b3bbeb
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-02-07 17:58:18 +0300

    small fix

--- scripts/get_androidsdk.sh
+++ scripts/get_androidsdk.sh
@@ -6,12 +6,8 @@ GROUP_ID=$2
 echo "Temp update. not save in image"
 apt-get -y update  
 apt-get -y upgrade 
-apt-get -y install wget curl unzip
+apt-get -y install curl unzip
     
-[[ -d /opt/android-sdk/android_openssl ]] || mkdir /opt/android-sdk/android_openssl
-    
-cp -r /usr/local/src/android_openssl/* /opt/android-sdk/android_openssl
-
 ANDROID_SDK_ROOT="/opt/android-sdk"
 ANDROID_NDK_ROOT="/opt/android-sdk/ndk"
 
@@ -24,15 +20,16 @@ sdkmanager --sdk_root=/opt/android-sdk "ndk;22.1.7171670";
 [[ -d /opt/android-sdk/cmdline-tools ]] || mkdir /opt/android-sdk/cmdline-tools 
 ln -fs /opt/cmdline-tools/ /opt/android-sdk/cmdline-tools/latest
 
-# Install ndk samples in ${ANDROID_NDK_ROOT}/samples
-cd ${ANDROID_NDK_ROOT}
+echo "Install ndk samples in ${ANDROID_NDK_ROOT}/samples"
+
+cd /tmp
 curl -sLO https://github.com/android/ndk-samples/archive/master.zip
 unzip -q master.zip 
-rm -v master.zip
-mv ndk-samples-master samples
+[[ -d ${ANDROID_NDK_ROOT}/samples ]] || mv ndk-samples-master ${ANDROID_NDK_ROOT}/samples
 
-# Download buildtool to generate aab packages in ${ANDROID_SDK_ROOT}
+echo "Download buildtool to generate aab packages in ${ANDROID_SDK_ROOT}"
+
 cd ${ANDROID_SDK_ROOT}
-curl -sLO https://github.com/google/bundletool/releases/download/1.3.0/bundletool-all-1.3.0.jar
+[[ -f ${ANDROID_SDK_ROOT}/bundletool-all-1.3.0.jar ]] || curl -sLO https://github.com/google/bundletool/releases/download/1.3.0/bundletool-all-1.3.0.jar
     
 chown -R $USER_ID:$GROUP_ID /opt/android-sdk/
