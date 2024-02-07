72f6e27755be293da924b146f5c72786dd64f389
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-02-07 17:59:03 +0300

    build qt5 amd64 script

--- scripts/build_qt5_amd64.sh
+++ scripts/build_qt5_amd64.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-
-pushd .
-trap "popd" EXIT HUP INT QUIT TERM
 
 VERSION=$1
 USER_ID=$2
 GROUP_ID=$3
+DEBUG_MODE=$4
+QT_PATH="/opt/Qt/${VERSION}"
 
-mkdir -p /tmp/build_qt5 
-cd /tmp/build_qt5 
+[[ "$DEBUG_MODE" != "y" ]] ||  rm -R ${QT_PATH}/build_qt
+[[ -d ${QT_PATH}/build_qt ]] || mkdir ${QT_PATH}/build_qt
+
+cd ${QT_PATH}/build_qt
 
 #Solution is to configure qt with -feature-freetype -fontconfig to allow the use of system fonts
 #set QT_QPA_FONTDIR to something suitable, f.ex. /usr/share/fonts/truetype/dejavu (do export QT_QPA_FONTDIR=/usr/share/fonts/truetype/dejavu before running the application)
 
-/usr/local/src/qt5/configure -opensource -confirm-license -dbus -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -release -feature-freetype -fontconfig \
+/usr/local/src/qt5/configure -opensource -confirm-license -dbus -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -release -feature-freetype  \
 -reduce-relocations -optimized-qmake -nomake tests -nomake examples -no-feature-d3d12 -skip 3d -skip activeqt -skip canvas3d -skip datavis3d -skip doc -skip gamepad -skip qtdocgallery \
 -skip lottie -skip macextras -skip charts -skip quick3d -skip script -skip scxml -skip speech -skip virtualkeyboard -skip qtwebengine -skip webchannel -skip webengine \
--skip webglplugin -skip websockets -skip webview -skip winextras -prefix /opt/Qt/${VERSION} -v -pkg-config
+-skip webglplugin -skip websockets -skip webview -skip winextras -prefix ${QT_PATH}/build_qt -v -pkg-config
 
 make -j $(nproc) &> make.log
 make -j $(nproc) install 
+ 
+cp make.log ${QT_PATH}/build_qt/make.log
+cp config.summary ${QT_PATH}/build_qt/config.summary 
+chown -R $USER_ID:$GROUP_ID ${QT_PATH}/build_qt
 
-cp make.log /opt/Qt/$VERSION/make.log
-cp config.summary /opt/Qt/$VERSION/config.summary 
-chown -R $USER_ID:$GROUP_ID /opt/opt/Qt/$VERSION/
-
-popd >& /dev/null
