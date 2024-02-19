dca2e4f41d2a374aa8bd83b8785d33e78cf806e4
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-02-19 10:59:43 +0300

    update QtCreator version to v13.0.0

--- scripts/run_qtcreator_docker.sh
+++ scripts/run_qtcreator_docker.sh
@@ -16,7 +16,7 @@ SRC_VOLUME_NAME="${QT_VERSION}-src-volume"
 SDK_VOLUME_NAME="${QT_VERSION}-android-sdk-volume"
 QT5_OPT_VOLUME_NAME="${QT_VERSION}-opt-volume"
 TOOLCHAIN_IMAGE_NAME="zanyxdev/qt5-toolchain:${QT_VERSION}" 
-QTCREATOR_IMAGE_NAME="zanyxdev/qt5-qtcreator:v12.0.0" 
+QTCREATOR_IMAGE_NAME="zanyxdev/qt5-qtcreator:v13.0.0" 
 CCACHE_VOLUME="${QT_VERSION}-ccache-volume"
 
 [[ -d "$HOME"/docker_dev_home ]] || mkdir "$HOME"/docker_dev_home
