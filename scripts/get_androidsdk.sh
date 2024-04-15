06ff4fddb5e28d96d3939ade1b9bfaa9c8a9c25a
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2024-04-15 07:09:46 +0300

    add build tools

--- scripts/get_androidsdk.sh
+++ scripts/get_androidsdk.sh
@@ -10,6 +10,7 @@ ANDROID_NDK_ROOT="/opt/android-sdk/ndk"
 
 cd /opt  
 yes | sdkmanager --sdk_root=/opt/android-sdk --licenses
+sdkmanager --sdk_root=/opt/android-sdk "platforms;android-30" "platform-tools" "build-tools;30.0.2"; 
 sdkmanager --sdk_root=/opt/android-sdk "platforms;android-31" "platform-tools" "build-tools;31.0.0"; 
 sdkmanager --sdk_root=/opt/android-sdk --list; 
 sdkmanager --sdk_root=/opt/android-sdk "ndk;22.1.7171670"; 
