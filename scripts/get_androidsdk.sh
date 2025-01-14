3af365ab6cf101e819e508e154111e3cbd0d7191
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2025-01-14 13:30:25 +0300

    add ndk and cmdline-tools

--- scripts/get_androidsdk.sh
+++ scripts/get_androidsdk.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd /opt  
 yes | sdkmanager --sdk_root=/opt/android-sdk --licenses
+sdkmanager --sdk_root=/opt/android-sdk "cmdline-tools;latest"
 sdkmanager --sdk_root=/opt/android-sdk "platforms;android-30" "platform-tools" "build-tools;30.0.2"; 
 sdkmanager --sdk_root=/opt/android-sdk "platforms;android-31" "platform-tools" "build-tools;31.0.0"; 
 sdkmanager --sdk_root=/opt/android-sdk --list; 
+sdkmanager --sdk_root=/opt/android-sdk "ndk;21.3.6528147"; 
 sdkmanager --sdk_root=/opt/android-sdk "ndk;22.1.7171670"; 
 sdkmanager --sdk_root=/opt/android-sdk "ndk;25.1.8937393";    
 chown -R $USER_ID:$GROUP_ID /opt/android-sdk/
